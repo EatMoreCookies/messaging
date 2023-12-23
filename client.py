d3f06ed527a52a2d17f6fa6b2e7586adef00868f
natef <natefagrey@gmail.com>
2023-12-23 16:01:22 -0700

    Cleaned up

--- client.py
+++ client.py
@@ -12,9 +12,10 @@ args = parser.parse_args()
 from_client_id = int(args.from_client_id[0])
 to_client_id = int(args.to_client_id[0])
 
-def send_message_to_server(msg: str):
+def send_message_to_server(msg: str, type: str):
     with connect("ws://localhost:8765") as websocket:
         message_json: dict = {
+            "type": type,
             "to_id": to_client_id,
             "from_id": from_client_id,
             "body": msg 
@@ -22,10 +23,14 @@ def send_message_to_server(msg: str):
 
         websocket.send(json.dumps(message_json))
         response = websocket.recv()
+        
+        print(f"\r> {response}")
 
-        print(f"< {response}")
+message = ""
+type = "__connect__"
 
 while True:
-    message = input("> ")
-    send_message_to_server_thread = threading.Thread(target=send_message_to_server, args=(message,))
+    message = input()
+    type = "__chat__"
+    send_message_to_server_thread = threading.Thread(target=send_message_to_server, args=(message, type,))
     send_message_to_server_thread.start()
--- server.py
+++ server.py
@@ -3,9 +3,6 @@ from websockets.server import serve
 from datetime import datetime
 import json
 
-to_ids = []
-from_ids = []
-
 connections = {}
 
 async def echo(websocket):
@@ -15,13 +12,17 @@ async def echo(websocket):
         from_id = message_json['from_id']
         to_id = message_json['to_id']
         message = message_json['body']
+        type = message_json['type']
 
         connections[from_id] = websocket
 
         response_msg = f"Recieved message {message} from {from_id} to {to_id} at {datetime.now()}. {len(connections)} connections."
         print(response_msg)
 
+        # await websocket.send("__message__received__")
+
         if to_id in connections:
+            print(f"Sending message from {from_id} to {to_id}")
             await connections[to_id].send(message)
             
 async def main():
